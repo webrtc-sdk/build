a48057e00978596ddf282ee3b4501587bc62c886
Jon Kunkee <jkunkee@microsoft.com>
2019-01-07 12:15:51 -0800

    Fix CDB.exe copy when targeting ARM64 Windows
    This change adds ARM64 support to the CDB.exe copy script. Also, since
    ARM64 Windows is by definition Windows 10 1709+, the eventing-provider
    and ucrtbase DLLs are shipped with the OS and do not need to be copied.
    
    See also
    https://bugs.chromium.org/p/v8/issues/detail?id=8645
    
    Bug: 893460
    Change-Id: I8083b5b93973d2766f8abcd1a726138c67aac027

--- win/copy_cdb_to_output.py
+++ win/copy_cdb_to_output.py
@@ -66,8 +66,8 @@ def _CopyCDBToOutput(output_dir, target_arch):
                                         '\\Windows Kits\\10')))
   if target_arch == 'ia32' or target_arch == 'x86':
     src_arch = 'x86'
-  elif target_arch == 'x64':
-    src_arch = 'x64'
+  elif target_arch in ['x64', 'arm64']:
+    src_arch = target_arch
   else:
     print 'copy_cdb_to_output.py: unknown target_arch %s' % target_arch
     sys.exit(1)
@@ -99,10 +99,11 @@ def _CopyCDBToOutput(output_dir, target_arch):
   _CopyImpl('uext.dll', dst_winext_dir, src_winext_dir)
   _CopyImpl('exts.dll', dst_winxp_dir, src_winxp_dir)
   _CopyImpl('ntsdexts.dll', dst_winxp_dir, src_winxp_dir)
-  _CopyImpl('api-ms-win-eventing-provider-l1-1-0.dll', output_dir, src_dir)
+  if src_arch in ['x64', 'x86']:
+    _CopyImpl('api-ms-win-eventing-provider-l1-1-0.dll', output_dir, src_dir)
+    _CopyImpl('ucrtbase.dll', output_dir, src_crt_dir)
   for dll_path in glob.glob(os.path.join(src_crt_dir, 'api-ms-win-*.dll')):
     _CopyImpl(os.path.split(dll_path)[1], output_dir, src_crt_dir)
-  _CopyImpl('ucrtbase.dll', output_dir, src_crt_dir)
   return 0
 
 
